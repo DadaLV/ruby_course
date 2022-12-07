9daa9ea9add2c3f7377bee151a12d6e50ea5e9ba
DadaLV <vynohradova.l.a@gmail.com>
2022-12-07 02:49:17 +0200

    test

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,12 @@
-def say_hello(name, city, state)
-  "Hello, #{name.join(" ")}! Welcome to #{city}, #{state}!"
-end
+# def say_hello(name, city, state)
+#   "Hello, #{name.join(" ")}! Welcome to #{city}, #{state}!"
+# end
 
 # puts say_hello(['John', 'Smith'], 'Phoenix', 'Arizona')
 
-puts ['John', 'Smith'].join(" ")
+# puts ['John', 'Smith'].join(" ")
+
+# puts 1.is_a? Integer
+
+puts Integer.sqrt(9) == Math.sqrt(9)
+# .integer?
