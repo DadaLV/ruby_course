9553439ba714eccf0bea33821a1fe5cb8b6bf0f7
DadaLV <vynohradova.l.a@gmail.com>
2023-01-03 15:22:08 +0200

    tasks added

--- site.rb
+++ site.rb
@@ -29,57 +29,68 @@
 # puts a1.current_url
 # class Site
 #   HOME_URL = 'https://www.home.com'
-#   attr_reader :current_url
-  
+#   attr_reader :current_url  
+
 #   def initialize
 #     @current_url = HOME_URL
 #   end
 
 #   def go_to(relevant_path)
-#     relevant_path == @current_url ? @current_url : nil
-#    end
+#     relevant_path = "#{HOME_URL}/#{relevant_path}"
+#     relevant_path != @current_url ? @current_url = relevant_path : nil     
+#   end
   
 #   def to_s
-#     "*----------*\nhome: #{HOME_URL}\ncurrent url: #{current_url}\n*----------*"
+#     "*----------*\n home: #{HOME_URL}\n current url: #{current_url}\n*----------*"
 #   end
 # end
 
 class Site
   HOME_URL = 'https://www.home.com'
   attr_reader :current_url
-  @path
-  @history  
+  @@history = []
+ 
 
   def initialize
     @current_url = HOME_URL
-    history = []
-    @history = history
-    
+    @@history.push(@current_url)
   end
 
   def go_to(relevant_path)
-    
     relevant_path = "#{HOME_URL}/#{relevant_path}"
-    relevant_path == @current_url ? nil : @current_url = relevant_path
-    @history.push(@current_url)    
+    
+    if relevant_path != @current_url
+      @current_url = relevant_path
+      @@history.push(@current_url)
+    else
+      nil  
+    end        
   end
 
   def <(other)
-    @history < other.history
+    @current_url.index < other.current_url.index
   end
 
   def >(other)
-    @history[1] > history[0]
+  if other.is_a? Site
+   @current_url.index > other.current_url.index
+    else
+    false
+    end
   end
 
   def ==(other)
-    @history[1] == history[0]
+    @current_url.index == other.current_url.index
   end
-
+  
+  def !=(other)
+    @current_url.index != other.current_url.index
+  end
     
   
   def to_s
-    "*----------*\nhome: #{HOME_URL}\ncurrent url: #{current_url}\nhistory: #{history}*----------*"
+    "*----------*\n home: #{HOME_URL}\n current url: #{current_url}\n history: #{@@history}\n*----------*"
+    # puts @current_url.index
   end
 end
 
@@ -89,3 +100,58 @@ a1.go_to('2')
 a2 = Site.new
 a2.go_to('1')
 puts(a1.history)
+
+# class Site
+#   HOME_URL = 'https://www.home.com'
+#   attr_reader :current_url
+#   @@history = []
+ 
+
+#   def initialize
+    
+#     @current_url = HOME_URL
+#     @@history.push(@current_url)
+#   end
+
+#   def go_to(relevant_path)
+    
+#     relevant_path = "#{HOME_URL}/#{relevant_path}"
+#     if (relevant_path == @current_url)
+#       nil
+      
+#     else
+#     @current_url = relevant_path
+    
+#     end
+#     if relevant_path == @current_url
+#             @current_url
+#         else
+#             @current_url = "#{HOME_URL}/#{home}"
+#         end
+        
+#         @@history.push(@current_url)
+    
+#   end
+
+#   def <(other)
+#     @current_url.index < other.current_url.index
+#   end
+
+#   def >(other)
+#   if other.is_a? Site
+#     @current_url.index > other.current_url.index
+#     else
+#     false
+#     end
+#   end
+
+#   def ==(other)
+#     @current_url.index == other.current_url.index
+#   end
+    
+  
+#   def to_s
+#     "*----------*\n home: #{HOME_URL}\n current url: #{current_url}\n history: #{@history}\n*----------*"
+#   end
+# end
+
